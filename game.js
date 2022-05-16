6c0e9c0c6f2bbf2f7fcab71511e5cbd0aaf0f393
Dimitri Bertolami <dimibertolami@gmail.com>
2022-05-16 14:43:44 +0200

    multipliers apear to be working

--- game.js
+++ game.js
@@ -1,10 +1,12 @@
 let counter=0
-
+let multiplier = 0;
 document.getElementById("upgrade").addEventListener("click", function() {
     if(counter>=20 && document.getElementById("upgrade2").hidden === true){
-            counter = counter - 20;
-            document.getElementById("counter").innerHTML = counter;
-            document.getElementById("upgrade2").hidden = false;
+        counter = counter - 20;
+        document.getElementById("counter").innerHTML = counter;
+        document.getElementById("upgrade2").hidden = false;
+        document.getElementById("multiplierID").textContent = "cookie (x2)";
+        multiplier = 2;
     } else {
         document.getElementById("upgrade").disabled = true;
     }
@@ -16,6 +18,8 @@ document.getElementById("upgrade2").addEventListener("click", function() {
         document.getElementById("counter").innerHTML = counter;
         document.getElementById("upgrade3").disabled = false;
         document.getElementById("upgrade").disabled = true;
+        document.getElementById("multiplierID").textContent = "cookie (x4)";
+        multiplier = 4;
     } else {
         document.getElementById("upgrade").disabled = true;
     }
@@ -25,6 +29,10 @@ document.getElementById("upgrade3").addEventListener("click", function() {
     if(counter>=60){
         counter = counter - 60;
         document.getElementById("counter").innerHTML = counter;
+        document.getElementById("upgrade").disabled = true;
+        document.getElementById("upgrade2").disabled = true;
+        document.getElementById("multiplierID").textContent = "cookie (x10)";
+        multiplier = 10;
     } else {
         document.getElementById("upgrade").disabled = true;
     }
@@ -32,7 +40,15 @@ document.getElementById("upgrade3").addEventListener("click", function() {
 
 function addCookie(){
     let cookie = document.getElementById("counter").value;
-    counter++
+    if(multiplier===0){
+        counter++;
+    } else if(multiplier===2){
+        counter=counter+2;
+    } else if(multiplier===4){
+        counter=counter+4
+    } else if(multiplier===10){
+        counter=counter+10
+    }
     document.getElementById("counter").innerHTML = counter;
     if(counter===20){
         document.getElementById("upgrade").disabled = false;
