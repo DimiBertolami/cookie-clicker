eb8776d3f82548a56acd6df33390c8b5beaa5373
Dimitri Bertolami <dimibertolami@gmail.com>
2022-05-16 12:34:26 +0200

    bootstrap all the way.. buttons upgrades etc implemented but still alot of work to do

--- game.js
+++ game.js
@@ -1,5 +1,16 @@
 let counter=0
 
+
+
+
+document.getElementById("upgrade").addEventListener("click", function() {
+    if(counter>=20){
+        counter = counter - 20;
+        document.getElementById("counter").innerHTML = counter;
+        document.getElementById("upgrade2").hidden = false;
+    }
+});
+
 function addCookie(){
     let cookie = document.getElementById("counter").value;
     counter++
@@ -7,4 +18,10 @@ function addCookie(){
     if(counter===20){
         document.getElementById("upgrade").disabled = false;
     }
+    if (document.getElementById("upgrade").hidden === false){
+        if(counter===40){
+            document.getElementById("upgrade2").disabled = false;
+        }
+    }
+
 }
