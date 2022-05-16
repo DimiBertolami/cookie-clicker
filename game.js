39f6f99ab08b2eb0b483148a0dbae876bb6a3528
Dimitri Bertolami <dimibertolami@gmail.com>
2022-05-16 14:08:41 +0200

    working on eventlisteners in combination with the addcookie function

--- game.js
+++ game.js
@@ -1,13 +1,32 @@
 let counter=0
 
-
-
-
 document.getElementById("upgrade").addEventListener("click", function() {
-    if(counter>=20){
-        counter = counter - 20;
+    if(counter>=20 && document.getElementById("upgrade2").hidden === true){
+            counter = counter - 20;
+            document.getElementById("counter").innerHTML = counter;
+            document.getElementById("upgrade2").hidden = false;
+    } else {
+        document.getElementById("upgrade").disabled = true;
+    }
+});
+
+document.getElementById("upgrade2").addEventListener("click", function() {
+    if(counter>=40){
+        counter = counter - 40;
         document.getElementById("counter").innerHTML = counter;
-        document.getElementById("upgrade2").hidden = false;
+        document.getElementById("upgrade3").disabled = false;
+        document.getElementById("upgrade").disabled = true;
+    } else {
+        document.getElementById("upgrade").disabled = true;
+    }
+});
+
+document.getElementById("upgrade3").addEventListener("click", function() {
+    if(counter>=60){
+        counter = counter - 60;
+        document.getElementById("counter").innerHTML = counter;
+    } else {
+        document.getElementById("upgrade").disabled = true;
     }
 });
 
@@ -18,10 +37,18 @@ function addCookie(){
     if(counter===20){
         document.getElementById("upgrade").disabled = false;
     }
-    if (document.getElementById("upgrade").hidden === false){
+    if (document.getElementById("upgrade2").hidden === false){
         if(counter===40){
             document.getElementById("upgrade2").disabled = false;
+            document.getElementById("upgrade").disabled = true;
+            document.getElementById("upgrade3").hidden  = false;
         }
     }
-
+    if (document.getElementById("upgrade3").hidden === false){
+        if(counter===60){
+            document.getElementById("upgrade").disabled = false;
+            document.getElementById("upgrade2").disabled = false;
+            document.getElementById("upgrade3").disabled  = false;
+        }
+    }
 }
